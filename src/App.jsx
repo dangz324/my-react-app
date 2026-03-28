dc56ce9a36482f9966cd2f36d0dbac9d660152e6
Zachary Dang <dangz@wustl.edu>
2026-03-28 15:05:32 -0500

    squares display x when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -125,7 +125,8 @@ import './App.css'
 function Square() {
   const [value, setValue] = useState(null);
   function handleClick() {
-    console.log({value}, 'clicked!');
+    // console.log({value}, 'clicked!');
+    setValue('X');
   }
 
   return <button className="square" onClick={handleClick}>{ value }</button>;
@@ -135,19 +136,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
